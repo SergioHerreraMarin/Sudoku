2ba225d4285c7c1cd6c498832d9ab17d0e78b42c
Sergio <sergioherreramarin@gmail.com>
2022-10-24 12:22:39 +0200

    Updated Sudoku spinners

--- app/src/main/java/com/example/sudoku/MainActivity.java
+++ app/src/main/java/com/example/sudoku/MainActivity.java
@@ -5,17 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.TableLayout;
 import android.widget.TableRow;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private TableLayout tableLayout;
-    private Context context;
     private SudokuModel model;
 
     @Override
@@ -23,8 +24,8 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        model = new SudokuModel();
 
+        model = new SudokuModel();
         tableLayout = findViewById(R.id.tableLayout);
         createTable(model.getSudokuData());
     }
@@ -39,32 +40,28 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
 
     private void createTable(int[][] data){
-        for(int i = 0; i < data.length; i++){
+        for(int x = 0; x < data.length; x++){
             TableRow row = new TableRow(this.getApplicationContext());
-            for(int j = 0; j < data[i].length; j++){
+            for(int y = 0; y < data[x].length; y++){
                 Spinner spinner = new Spinner(this);
+                spinner.setBackground(null);
+                spinner.setBackgroundColor(Color.LTGRAY);
                 ArrayAdapter<CharSequence> adapter = new ArrayAdapter<CharSequence>(this,
-                        android.R.layout.simple_spinner_item, model.getNombres());
+                        android.R.layout.simple_spinner_item, model.getOpciones());
                 spinner.setAdapter(adapter);
-
-                //spinner.setBackground(null);
-                //spinner.setPadding(5,5,5,5);
-                //spinner.setBackgroundColor(Color.BLUE);
-                spinner.setTag(R.id.col, j);
-                spinner.setTag(R.id.fila, i);
                 spinner.setOnItemSelectedListener(this);
-                row.addView(spinner, newTableRowParams());
+                spinner.setTag(R.id.col, y);
+                spinner.setTag(R.id.fila, x);
+                row.addView(spinner);
             }
             tableLayout.addView(row);
         }
     }
 
 
-
-
     @Override
     public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-        adapterView.getItemAtPosition(i);
+        model.setVal((int)adapterView.getTag(R.id.fila),(int)adapterView.getTag(R.id.col), (int)adapterView.getItemAtPosition(i));
     }
 
     @Override
--- app/src/main/java/com/example/sudoku/SudokuModel.java
+++ app/src/main/java/com/example/sudoku/SudokuModel.java
@@ -1,20 +1,78 @@
 package com.example.sudoku;
 
 import android.content.Context;
+import android.os.Debug;
+import android.util.Log;
 import android.widget.ArrayAdapter;
 import android.widget.TableLayout;
 
 public class SudokuModel {
 
     private static int[][] sudokuData = new int[9][9];
-    CharSequence[] nombres = {"1","2","3","4","5","6","7","8","9"};
+    CharSequence[] opciones = {"1","2","3","4","5","6","7","8","9"};
 
     public int[][] getSudokuData(){
         return sudokuData;
     }
 
-    public CharSequence[] getNombres(){
-        return this.nombres;
+    public CharSequence[] getOpciones(){
+        return this.opciones;
+    }
+
+    public int getVal(int x, int y){
+        return sudokuData[x][y];
+    }
+
+    public void setVal(int x, int y, int val){
+        if(comprovaFila(x,y) && comprovaColumna(x,y)){
+            sudokuData[x][y] = val;
+        }
+        Log.i("Incorrecto", "Incorrecto2");
+    }
+
+    public boolean comprovaFila(int x, int y){
+
+        boolean correcto = true;
+        int currentNum;
+
+        for(int i = 0; i < sudokuData.length; i++){
+            currentNum = getVal(i, y);
+            for(int j = 0; j < sudokuData.length; j++){
+                if(getVal(j, y) == currentNum){
+                    if(j != i){
+                        correcto = false;
+                    }
+                }
+            }
+        }
+        return correcto;
+    }
+
+    public boolean comprovaColumna(int x, int y){
+
+        boolean correcto = true;
+        int currentNum;
+
+        for(int i = 0; i < sudokuData.length; i++){
+            currentNum = getVal(x, i);
+            for(int j = 0; j < sudokuData.length; j++){
+                if(getVal(x, j) == currentNum){
+                    if(j != i){
+                        correcto = false;
+                    }
+                }
+            }
+        }
+        return correcto;
+    }
+
+    public boolean comprovaQuadrant(int x, int y){
+
+        return true;
+    }
+
+    public void creaPartida(){
+
     }
 
 
